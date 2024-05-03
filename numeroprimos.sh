06c7810ada871d16b336eb09262a95cf3ae06090
Cynthia Ramayo Ake <230300739@ucaribe.edu.mx>
2024-05-03 12:27:54 -0500

    easy

---
+++ numeroprimos.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Función para verificar si un número es primo
+is_prime() {
+    local number=$1
+    if [ $number -le 1 ]; then
+        return 1
+    fi
+    if [ $number -eq 2 ]; then
+        return 0
+    fi
+    if [ $(($number % 2)) -eq 0 ]; then
+        return 1
+    fi
+    local i=3
+    while [ $((i * i)) -le $number ]; do
+        if [ $(($number % i)) -eq 0 ]; then
+            return 1
+        fi
+        i=$((i + 2))
+    done
+    return 0
+}
+
+# Imprimir todos los números primos entre 60000 y 63000
+printf "Números primos entre 60000 y 63000:\n"
+for ((num = 60000; num <= 63000; num++)); do
+    if is_prime $num; then
+        printf "%d\t" $num
+    fi
+done
+printf "\n"
+
